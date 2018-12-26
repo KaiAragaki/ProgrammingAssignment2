91af586fb545004647292c2e07174408e17961f4
KaiAragaki <adamaragaki@gmail.com>
2018-12-26 09:47:17 -0500

    Added function content

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Contains a function that create a special matrix object with functions for setting and getting contents and inverses. Also contains a function that can retrieve a cached inverse, or, if unavailable, can solve for one.
 
-## Write a short comment describing this function
-
+# Takes a matrix and produces an object with several functions available to it: - Set, to set matrix contents; Get, to get matrix contents; getInverse, to get the inverted matrix; and setInverse, to set the inveted matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+        }
+        get <- function() x
+        getInverse <- function() i <<- solve(x)
+        setInverse <- function(inv) i <<- inv
+        list(set = set, get = get, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+# Solves for the inverse of the matrix, unless there is already a cached inverse - at which point it retrieves it without solving redundantly.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse()
+        if(!is.null(i)) {
+                message("Data was cached, retrieving")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setInverse(i)
+        i
 }
